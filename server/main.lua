eee5289c647c869f69128e17d2ebd731fff15941
Zesk <116458300+ZeskGarcia@users.noreply.github.com>
2023-11-07 08:31:42 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -8,14 +8,10 @@ local function onPlayerConnecting(name, setKickReason, deferrals)
   Wait(0)
   deferrals.update(string.format(Translations['firstDeferralsObject'], name))
   for _, v in pairs(identifiers) do
-    if (string.find(v, "steam:")) then
-      if (identifier['steam'] == nil) then
-        identifier['steam'] = v
-      end
-    elseif (string.find(v, "license:")) then
-      if (identifier['license'] == nil) then
-        identifier['license'] = v
-      end
+    if (string.find(v, "steam:") and identifier['steam'] == nil) then
+      identifier['steam'] = v
+    elseif (string.find(v, "license:") and identifier['license'] == nil) then
+      identifier['license'] = v
     end
   end
 end
